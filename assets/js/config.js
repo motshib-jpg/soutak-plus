6ffbfc9f608a4deb630856d60fcab266f3517c60
motshib-jpg <mot.shib@gmail.com>
2026-09-07 03:05:33 +0300

    Disable first-party browser analytics collection

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,7 +2,7 @@ window.SOUTAK_CONFIG = {
   supabaseUrl: "https://vvrdchwlmwpbsxsmcthp.supabase.co",
   supabasePublishableKey: "sb_publishable_oIOIyaajAgqJQ_Nb4EJ3pA_fc-EUaTM",
   siteUrl: "https://soutak-plus.vercel.app",
-  analyticsEnabled: true,
+  analyticsEnabled: false,
   ads: {
     enabled: false,
     provider: "adsense",
